6ac1bc05c2e28dbdba4f5b2f80f04209741f1de0
Krishna Ahire <krishnaahire24@gmail.com>
2025-05-06 16:43:41 +0530

    modified user route

--- routes/api/user.js
+++ routes/api/user.js
@@ -8,7 +8,7 @@ const authorizeRoleMiddleware = require('../../middleware/authorizeRole.middlewa
 router.get('/', authMiddleWare, userController.getAllUsers)
       .post('/', authMiddleWare, authorizeRoleMiddleware('admin'), upload.single("profilePicture"), userController.createUser)
       .get('/:id',authMiddleWare, userController.getUserDetails)
-      .patch('/:id', authMiddleWare, upload.single("profilePicture"), userController.updateUser)
+      .put('/:id', authMiddleWare, upload.single("profilePicture"), userController.updateUser)
       .delete('/:id',authMiddleWare, authorizeRoleMiddleware('admin'), userController.deleteUser)
 
 module.exports = router;
